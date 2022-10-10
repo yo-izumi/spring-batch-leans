8a9be7f4d0b3f76c1da46cc99fcd8e44c0a1f624
yo-izumi <teru_izm_0510@yahoo.co.jp>
2022-10-10 13:26:27 +0900

    [update]postgresのdockerイメージをdocker-composeを使うように修正。 ワンパス形態のバッチに変更。properties設定も変更。

--- src/main/java/com/example/open_meteo_download/BatchLauncher.java
+++ src/main/java/com/example/open_meteo_download/BatchLauncher.java
@@ -1,34 +1,34 @@
-package com.example.open_meteo_download;
+// package com.example.open_meteo_download;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecutionException;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+// import org.springframework.batch.core.Job;
+// import org.springframework.batch.core.JobExecutionException;
+// import org.springframework.batch.core.JobParameters;
+// import org.springframework.batch.core.launch.JobLauncher;
+// import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
+// import lombok.RequiredArgsConstructor;
 
-@Component
-@EnableScheduling
-@RequiredArgsConstructor
-public class BatchLauncher {
-    private final JobLauncher jobLauncher;
-    private final Job openMeteoDownloadJob;
-    private final Job timeLogJob;
+// @Component
+// @EnableScheduling
+// @RequiredArgsConstructor
+// public class BatchLauncher {
+//     private final JobLauncher jobLauncher;
+//     private final Job openMeteoDownloadJob;
+//     private final Job timeLogJob;
 
-    private JobParameters params;
+//     private JobParameters params;
 
-    // 日曜日09:00:00に実行
-    @Scheduled(cron ="${batchlauncher.cron}")
-    public void launchJob() throws JobExecutionException {
-        this.params = openMeteoDownloadJob.getJobParametersIncrementer().getNext(this.params);
-        this.jobLauncher.run(this.openMeteoDownloadJob, this.params);
-    }
+//     // 日曜日09:00:00に実行
+//     @Scheduled(cron ="${batchlauncher.cron}")
+//     public void launchJob() throws JobExecutionException {
+//         this.params = openMeteoDownloadJob.getJobParametersIncrementer().getNext(this.params);
+//         this.jobLauncher.run(this.openMeteoDownloadJob, this.params);
+//     }
 
-    public void timeLogJob() throws JobExecutionException {
-        this.params = timeLogJob.getJobParametersIncrementer().getNext(this.params);
-        this.jobLauncher.run(this.timeLogJob, this.params);
-    }
-}
+//     public void timeLogJob() throws JobExecutionException {
+//         this.params = timeLogJob.getJobParametersIncrementer().getNext(this.params);
+//         this.jobLauncher.run(this.timeLogJob, this.params);
+//     }
+// }
